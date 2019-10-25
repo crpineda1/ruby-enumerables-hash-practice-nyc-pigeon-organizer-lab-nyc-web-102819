443edc951b7bb681b0c655d0787c91e579601cf6
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-25 14:22:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,21 @@ def nyc_pigeon_organizer(data)
   # write your code here!
 new_hash = {}
 
-data.each do |key, value|
-binding.pry
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+
+        if !new_hash[name]
+          new_hash[name] = {}
+
+
+        end
+          if 
+      end
+
+    end
+
+  end
 
 end
 
